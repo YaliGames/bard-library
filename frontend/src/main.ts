d7d5715b1c34ef087f9281d8743ffee09b7da1d9
ZFengMo <mr_mini_fish@outlook.com>
2025-10-26 22:30:09 +0800

    refactor: use Chinese locale in element plus

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 import 'material-symbols/index.css'
 import './styles/reset.css'
@@ -12,5 +13,7 @@ import './styles/tailwind.css'
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.mount('#app')
